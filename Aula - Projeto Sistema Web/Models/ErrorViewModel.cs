76223b810d68f9edec8810fc45e7bb629f6b2512
Luan Gobo <luangobo@hotmail.com>
2024-06-10 21:40:44 -0300

    Project

---
+++ Aula - Projeto Sistema Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula___Projeto_Sistema_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
