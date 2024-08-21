fc9b3a009df65c37e2dc8355c85dc0a816c44028
Luan Gobo <luangobo@hotmail.com>
2024-08-21 16:48:17 -0300

    Seller details and eager loading

--- Aula - Projeto Sistema Web/Controllers/SellersController.cs
+++ Aula - Projeto Sistema Web/Controllers/SellersController.cs
@@ -59,5 +59,20 @@ namespace SalesWeb.Controllers
             _sallerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sallerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
