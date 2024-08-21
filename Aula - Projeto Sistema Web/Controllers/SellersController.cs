614935177230fc6d916ebf20faa0190996a3abe5
Luan Gobo <luangobo@hotmail.com>
2024-08-21 17:31:16 -0300

    Update seller and custom services exception

--- Aula - Projeto Sistema Web/Controllers/SellersController.cs
+++ Aula - Projeto Sistema Web/Controllers/SellersController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using SalesWeb.Services;
 using SalesWeb.Models;
 using SalesWeb.Models.ViewModels;
 using System.Runtime.CompilerServices;
+using SalesWeb.Services.Exceptions;
+using System.Reflection.Metadata;
 
 namespace SalesWeb.Controllers
 {
@@ -73,6 +76,44 @@ namespace SalesWeb.Controllers
             }
 
             return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sallerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments};
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,Seller seller) 
+        {
+           if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sallerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException) 
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
